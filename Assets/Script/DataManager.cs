6d1d9c0fd98d84293396d1e06035adac082407ab
Draxniel <daluisgarcia@gmail.com>
2019-12-28 00:10:33 -0400

    Se agregó script de control de datos de una escena a otra
    Se agregó script DataManager que guarda los datos de una escena a otra

---
+++ Assets/Script/DataManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataManager : MonoBehaviour
+{
+
+    public static DataManager manager;
+    private Dictionary<Stat, int> stats;
+    private int coins;
+
+    private void Awake()
+    {
+        if (manager == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            stats = new Dictionary<Stat, int>();
+            stats.Add(Stat.Health, 5);
+            stats.Add(Stat.Strength, 1);
+            stats.Add(Stat.Defense, 1);
+            coins = 0;
+            manager = this;
+        }else if (manager != this)
+        {
+            stats = manager.getStats();
+            coins = manager.getCoins();
+        }
+    }
+
+    private void Update()
+    {
+        manager.setStats(stats);
+        manager.setCoins(coins);
+    }
+
+    public Dictionary<Stat, int> getStats()
+    {
+        return stats;
+    }
+
+    public int getCoins()
+    {
+        return coins;
+    }
+
+    public void setStats(Dictionary<Stat, int> stat)
+    {
+        stats = stat;
+    }
+
+    public void setCoins(int coin)
+    {
+        coins = coin;
+    }
+}
