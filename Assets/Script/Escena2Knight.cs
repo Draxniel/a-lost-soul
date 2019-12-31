07989a2e9884ed75a49884bfb8538aa05418c70a
Cesh399 <cesaranez9@gmail.com>
2019-12-31 00:38:12 -0400

    cinematicas con heroe 2 sin necesidad de duplicarlas
    incluido inicio y prologo

--- Assets/Script/Escena2Knight.cs
+++ Assets/Script/Escena2Knight.cs
@@ -5,15 +5,29 @@ using UnityEngine.SceneManagement;
 
 public class Escena2Knight : MonoBehaviour
 {
+    public int skin;
+    public DataManager manager;
     // Start is called before the first frame update
     void Start()
     {
-        
+        skin = manager.getSkinNumber();
     }
 
     // Update is called once per frame
     void Update()
     {
+        skin = manager.getSkinNumber();
+        switch (skin)
+        {
+            case 1:
+                GetComponent<Animator>().SetBool("Hero2", false);
+                break;
+            case 2:
+                GetComponent<Animator>().SetBool("Hero2", true);
+                break;
+            case 3:
+                break;
+        }
         if (gameObject.transform.position.x < 210f)
         {
             gameObject.transform.position = new Vector3(gameObject.transform.position.x + 1.2f, gameObject.transform.position.y, gameObject.transform.position.z);
