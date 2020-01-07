3885e3a8d9d017223fbd289f1b6fb65a7dbc5d2c
ArtuKILL <39144443+ArtuKILL@users.noreply.github.com>
2020-01-06 21:58:12 -0400

    Update Level4Music.cs

--- Assets/Script/Level4Music.cs
+++ Assets/Script/Level4Music.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class Level4Music : MonoBehaviour
 {
+    public bool play;
+    public AudioClip clip;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+       play = true;
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (play)
+        {
+            Camera.main.GetComponent<AudioSource>().Stop();
+            Camera.main.GetComponent<AudioSource>().clip = clip;
+            Camera.main.GetComponent<AudioSource>().Play();
+            play = false;
+        }
+        ;
     }
 }
