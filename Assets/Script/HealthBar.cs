390e8e4ba0fd792500acb272d907d5f3a82abc3f
Cesh399 <cesaranez9@gmail.com>
2020-02-16 00:49:07 -0400

    barra de vida de enemigos y sprites

---
+++ Assets/Script/HealthBar.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public Slider slider;
+    
+    public void setHealth(int health)
+    {
+        slider.value = health;
+    }
+
+    public void setMaxHealth(int maxHealth)
+    {
+        slider.maxValue = maxHealth;
+        slider.value = maxHealth;
+    }
+    public void setActive(bool active)
+    {
+        gameObject.SetActive(active);
+    }
+}
